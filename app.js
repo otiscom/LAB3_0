00713d3c7d2675b13cdacb90cd412561cac493b7
otiscom <matlipi65@gmail.com>
2025-11-30 18:02:04 +0100

    Add controllers and models

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -12,7 +12,7 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,12 @@
-const path = require("path");
-const express = require("express");
+const express = require('express');
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  const price = Math.floor(Math.random() * 90) + 10;
-  const desc = "A very nice " + req.body.title;
-  products.push({ title: req.body.title, price: price, desc: desc });
-  res.redirect("/");
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
